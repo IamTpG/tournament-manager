976d8e91a82eee7feb11690d292020cb6b16e2e5
neidy254 <123thien54@gmail.com>
2025-07-03 18:42:21 +0700

    fix creating tournament

--- backend/models/Tournament.js
+++ backend/models/Tournament.js
@@ -4,7 +4,8 @@ const tournamentSchema = new mongoose.Schema ( {
     tour_ID :String,
     tour_name:String,   
     start_date :Date,
-    end_date: Date
+    end_date: Date,
+    game : String
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/tournamentDB',{
 app.post('/register',async(req,res) => 
 {
     try{
-        const {full_name, phone,ID, email,nick_name,tournament} = req.body;
-        const newPlayer = new Player({ full_name,phone,ID,email,nick_name,tournament,status :'pending' });  
+        
+        const {full_name, phone,ID, email,nick_name,register_tournament} = req.body;
+        const tour = await Tournament.find({tour_ID : register_tournament});
+        //check if we have that tournament
+        if (tour.length == 0){
+            return res.status(400).json({message:"Tournament does not exist or finished!"});
+        }
+
+        const newPlayer = new Player({ full_name,phone,ID,email,nick_name,register_tournament,status :'pending' });  
         await newPlayer.save();
 
         res.status(201).json({message: "Register Successfully!", player: newPlayer});
@@ -29,13 +36,29 @@ app.post('/register',async(req,res) =>
         res.status(500).json({error:'Error occured while creating registration ', details: err.message});
     }
 });
-// test-case, add 8 người giải đấu T01 và  tạo bảng xong check collection match 2/7/2025
+// http://localhost:3000/tournament
+app.post('/tournament',async(req,res) => {
+    try {
+    
+        const {tour_ID,tour_name,game}  = req.body; 
+        console.log(tour_ID);
+        const newTournament = new Tournament({tour_ID,tour_name,start_date :null,end_date : null,game});
+        console.log(newTournament.tour_ID);
+        await newTournament.save();
+        res.status(201).json({message:"Tournament created successfully",tournament:newTournament});
+    }
+    catch(error) {
+        res.status(500).json({error:'Errors happen creating bracket', details: error.message});
+    }
+}
+);
+// test-case, add 8 người giải đấu T001 và  tạo bảng xong check collection match 2/7/2025
 // http://localhost:3000/generate-matches
 app.post('/generate-matches', async(req,res)=>{
     try {
         const {tournamentID,numberOfPlayers} = req.body;
         
-        const players = await Player.find({tournament : tournamentID,status : 'pending'}).limit(numberOfPlayers);
+        const players = await Player.find({tournament : tournamentID,status : 'approved'}).limit(numberOfPlayers);
 
         
         if(players.length < numberOfPlayers) {
@@ -62,23 +85,6 @@ app.post('/generate-matches', async(req,res)=>{
         res.status(500).json({error:'Errors happen creating bracket',details: error.message});
     }
 });
-// http://localhost:3000/tournament
-app.post('/tournament',async(req,res) => {
-    try {
-        const {tourID,tourName,startDate,endDate}  = req.body;
-        const newTournament = new Tournament( {
-            tour_ID : tourID,
-            tourName: tourName,
-            start_date :startDate,
-            end_date : endDate
-        })
-        newTournament.save();
-        res.status(201).json({message:"Tournament created successfully",tournament:newTournament});
-    }
-    catch(error) {
-        res.status(500).json({error:'Errors happen creating bracket', details: error.message});
-    }
-}
-);
+
 const PORT = 3000;     
 app.listen(PORT, ()=>  console.log(`Server running at: http://localhost:${PORT}`));
