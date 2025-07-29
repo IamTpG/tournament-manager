c3aab96213015e91a54ec24a5fb6b4f409779923
neidy254 <123thien54@gmail.com>
2025-07-29 16:40:55 +0700

    remaking on match and tournament

--- backend/model/match.js
+++ backend/model/match.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose')
 
 const match_schema = new mongoose.Schema({
     
+
+    //result_list :String
     first_player: String,
     second_player: String,
+    // player list -> dua string vao tu tach thanh mang dong ?
     tournament_ID: String,
     occurence_day: {
         type: Date,
